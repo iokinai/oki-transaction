26691fbafc57a945c554a2874c1121bb8a01fcfc
okinai <okinaijap@gmail.com>
2023-11-25 04:32:32 +0300

    v2 fix

--- src/models/okirequest.rs
+++ src/models/okirequest.rs
@@ -3,5 +3,9 @@ use super::{EncryptedCardInfo, CBCAES256};
 
 #[derive(Serialize, Deserialize)]
 pub struct OkiRequest {
-    sender: EncryptedCardInfo<CBCAES256>
+    sender: EncryptedCardInfo<CBCAES256>,
+    receiver: EncryptedCardInfo<CBCAES256>,
+    amount: f64,
+    checksum: [u8; 32],
+    keysalt: [u8; 16],
 }
